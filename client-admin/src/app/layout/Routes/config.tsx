8e68067de60aad1995bd8487f31d6cedc388ab23
borisdovecer <36776986+borisdovecer@users.noreply.github.com>
2023-06-05 15:34:38 +0200

    routes

--- client-admin/src/app/layout/Routes/config.tsx
+++ client-admin/src/app/layout/Routes/config.tsx
@@ -3,6 +3,12 @@ import {
     ErrorPage,
     Home,
     Support,
+    Transfer,
+    ProductDetails,
+    ProductDefine,
+    Settings,
+    ProductList,
+    Accounts,
 } from "@app/pages";
 
 export interface IRoutes {
@@ -14,8 +20,13 @@ export interface IRoutes {
 export const routeConfig: IRoutes[] = [
     { id: 'home', path:'/', element: <Home /> },
     { id: 'dashboard', path:'/dashboard', element: <Dashboard /> },
+    { id: 'transfer', path:'/transfer', element: <Transfer /> },
+    { id: 'details', path:'/products/:id', element: <ProductDetails /> },
+    { id: 'define-product', path:'/define-product', element: <ProductDefine /> },
+    { id: 'settings', path:'/settings', element: <Settings /> },
+    { id: 'products', path:'/products', element: <ProductList /> },
+    { id: 'accounts', path:'/accounts', element: <Accounts /> },
     { id: 'error', path:'*', element: <ErrorPage /> }
-
 ];
 
 export const routeBasic: IRoutes[] = [

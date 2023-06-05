c6578fdfaa5635f6832302f07481375c37f280dc
borisdovecer <36776986+borisdovecer@users.noreply.github.com>
2023-06-05 13:17:07 +0200

    client app setup

---
+++ client-user/src/App.tsx
@@ -0,0 +1,11 @@
+
+const App = () =>  {
+
+    return (
+        <div className='h-screen pt-16 text-2xl bg-blue-400 text-center'>
+            user app
+        </div>
+    )
+}
+
+export default App
